bf57cd00914cd9916252554e260b9ef23611c6d1
Eyad Quraan <EyadQuraan@gmail.com>
2023-11-27 23:41:50 -0500

    Xpath

---
+++ src/com/loop/test/day4_tasks/Task1.java
@@ -0,0 +1,81 @@
+package com.loop.test.day4_tasks;
+
+import com.loop.test.utilities.DocuportConstants;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(DocuportConstants.URL_DOCUPORT_BETA);
+
+        WebElement inputUser = driver.findElement(By.xpath("//input[@type='text']"));
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
+        WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        inputUser.sendKeys(DocuportConstants.USERNAME_CLIENT);
+        inputPassword.sendKeys(DocuportConstants.PASSWORD_CLIENT);
+        button.click();
+
+        Thread.sleep(4000);
+        WebElement continueButton = driver.findElement(By.xpath("//span[text()=' Continue ']"));
+        continueButton.click();
+        Thread.sleep(3000);
+
+
+
+        List<WebElement> menuItems = driver.findElements(By.xpath("//span[@class='body-1']"));
+
+//        List<WebElement> menuItems = new ArrayList<>();
+//        menuItems.add(driver.findElement(By.xpath("//span[text()='Home']")));
+//        menuItems.add(driver.findElement(By.xpath("//span[text()='Received docs']")));
+//        menuItems.add(driver.findElement(By.xpath("//span[text()='My uploads']")));
+//        menuItems.add(driver.findElement(By.xpath("//span[text()='Invitations']")));
+        //menuItems.add(driver.findElement(By.xpath("//span[text()='1099 Form']")));
+
+        System.out.println("Directly on login: ");
+        for (int i = 0; i < menuItems.size(); i++) {
+
+            try{
+               System.out.println("\t\"" + menuItems.get(i).getText() + "\" " + ((menuItems.get(i).isDisplayed()) ? " is displayed" : " is not displayed"));
+
+            } catch (Exception e){
+                System.out.println("Element at " + i + " is not reachable");
+            }
+        }
+
+        WebElement menuButton = driver.findElement(By.xpath("//i[@class='v-icon notranslate mdi mdi-menu theme--light']"));
+        menuButton.click();
+
+        Thread.sleep(2000);
+
+         String[] items = {"Home", "Received docs", "My uploads", "Invitations", "1099 Form"};
+
+        System.out.println("After clicking on hamburger menu button" );
+        for (int i = 0; i < menuItems.size(); i++) {
+
+            try{
+                System.out.println("\t\"" + items[i] + "\" " + ((menuItems.get(i).isDisplayed()) ? " is displayed" : " is not displayed"));
+
+            } catch (Exception e){
+                System.out.println("Element at " + i + " is not reachable");
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
--- src/com/loop/test/utilities/DocuportConstants.java
+++ src/com/loop/test/utilities/DocuportConstants.java
@@ -2,12 +2,34 @@ package com.loop.test.utilities;
 
 public class DocuportConstants {
 
+    //Beta Login Credentials
     public static final String USERNAME_CLIENT = "b1g1_client@gmail.com";
     public static final String PASSWORD_CLIENT = "Group1";
 
-    public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
+    public static final String USERNAME_ADVISOR = "b1g1_advisor@gmail.com";
+    public static final String PASSWORD_ADVISOR = "Group1";
 
-    public static final String LOGO_DOCUPORT = "Docuport";
+
+
+
+    //Here are the menu item links in the beta version
+    public static final String URL_BETA_HOME = "https://beta.docuport.app/";
+    public static final String URL_BETA_RECEIVED_DOCS = "https://beta.docuport.app/received-docs";
+    public static final String URL_BETA_MY_UPLOADS= "https://beta.docuport.app/my-uploads";
+    public static final String URL_BETA_CLIENTS= "https://beta.docuport.app/clients";
+    public static final String URL_BETA_INVITATIONS = "https://beta.docuport.app/invitations";
+    public static final String URL_BETA_USERS= "https://beta.docuport.app/users";
+    public static final String URL_BETA_LEADS= "https://beta.docuport.app/leads";
+    public static final String URL_BETA_BOOKKEEPING = "https://beta.docuport.app/bookkeeping";
+    public static final String URL_BETA_1099_FORMS = "https://beta.docuport.app/1099-form";
+    public static final String URL_BETA_RECONCILIATIONS = "https://beta.docuport.app/reconciliations";
+
+
+
+
+
+    //Initial loading page
+    public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
 
     public static final String TYPE_OF_INPUT_USERNAME = "text";
     public static final String TYPE_OF_INPUT_PASSWORD = "password";
@@ -19,4 +41,13 @@ public class DocuportConstants {
 
     public static final String LOGIN_ERROR_ENTER_USERNAME = "Please enter your username or email address";
     public static final String LOGIN_ERROR_ENTER_PASSWORD = "Please enter your password";
+
+
+
+
+    //Other
+    public static final String LOGO_DOCUPORT = "Docuport";
+    public static final String URL_DOCUPORT_BETA= "https://beta.docuport.app/login";
+
+
 }
